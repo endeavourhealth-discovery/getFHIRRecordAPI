ac991744cc4f077f9cbd14859d4095ae730328c7
prash <prashanth.ponugoti@nhs.net>
2020-11-20 11:30:43 +0000

    /patient removed from URL

--- API/src/main/java/org/endeavourhealth/getFHIRRecordAPI/api/endpoints/CareRecordEndpoint.java
+++ API/src/main/java/org/endeavourhealth/getFHIRRecordAPI/api/endpoints/CareRecordEndpoint.java
@@ -16,7 +16,7 @@ import java.util.*;
 
 import static javax.ws.rs.client.Entity.form;
 
-@Path("patient")
+@Path("/")
 public class CareRecordEndpoint {
     private static final Logger LOG = LoggerFactory.getLogger(CareRecordEndpoint.class);
 
