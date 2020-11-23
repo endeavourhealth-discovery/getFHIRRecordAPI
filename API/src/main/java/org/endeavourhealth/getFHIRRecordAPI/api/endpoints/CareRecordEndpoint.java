88c3687c9c35d0a34a5b3ab0ccb6e35948949edc
prashanthpnhs <59019786+prashanthpnhs@users.noreply.github.com>
2020-11-23 11:38:51 +0000

    patient added in url

--- API/src/main/java/org/endeavourhealth/getFHIRRecordAPI/api/endpoints/CareRecordEndpoint.java
+++ API/src/main/java/org/endeavourhealth/getFHIRRecordAPI/api/endpoints/CareRecordEndpoint.java
@@ -16,7 +16,7 @@ import java.util.*;
 
 import static javax.ws.rs.client.Entity.form;
 
-@Path("/")
+@Path("patient")
 public class CareRecordEndpoint {
     private static final Logger LOG = LoggerFactory.getLogger(CareRecordEndpoint.class);
 
