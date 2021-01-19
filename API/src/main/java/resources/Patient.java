0c99dd36288ebf94ce3861c75988349d71bc5567
bearclawstu <stuart@bearclawsolutions.co.uk>
2021-01-19 08:23:56 +0000

    Set religion url

--- API/src/main/java/resources/Patient.java
+++ API/src/main/java/resources/Patient.java
@@ -45,9 +45,10 @@ public class Patient {
 		if (religionData != null) {
 			Extension extension1 = new Extension();
 			List<Coding> coding = new ArrayList<>();
-			coding.add(new Coding("https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-ReligiousAffiliation-1", religionData[0], religionData[1]));
+			coding.add(new Coding("http://hl7.org/fhir/StructureDefinition/patient-religion", religionData[0], religionData[1]));
 			CodeableConcept codeableConcept = (CodeableConcept) extension1.addChild("valueCodeableConcept");
 			codeableConcept.setCoding(coding);
+			extension1.setUrl("https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-ReligiousAffiliation-1");
 			patient.addExtension(extension1);
 		}
 
