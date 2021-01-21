f5fe50096663ddf56db729bc57bfd30589af54fc
bearclawstu <stuart@bearclawsolutions.co.uk>
2021-01-21 15:15:19 +0000

    Add all the config scripts and set gender to unknown in test mode

--- API/src/main/java/org/endeavourhealth/getFHIRRecordAPI/api/endpoints/FhirApi.java
+++ API/src/main/java/org/endeavourhealth/getFHIRRecordAPI/api/endpoints/FhirApi.java
@@ -302,7 +302,7 @@ public class FhirApi {
         patient.setAdduse(murphString);
         patient.setCity(murphString);
         patient.setDod(new Date("09-Sep-9999"));
-        patient.setGender("unknown");
+        patient.setGender("Unknown");
 
         return patient;
 
